9a8ae37bae947f5c654f4e32eae9e8d9200db9bf
Agostinho Sam'l <agostinhosaml832@gmail.com>
2025-02-10 10:08:33 +0100

    fix: parse username json data

--- src/components/BetsApp/utils/syncLocalStorage.ts
+++ src/components/BetsApp/utils/syncLocalStorage.ts
@@ -5,7 +5,9 @@ import { localStateKey } from "./localStateKey";
 export const syncLocalStorage = async () => {
   const apiUrl = "http://plan-150-bets.atwebpages.com";
   const data = JSON.parse(localStorage.getItem(localStateKey("bets")) || "{}");
-  const username = localStorage.getItem(localStateKey("username"));
+  const username = JSON.parse(
+    localStorage.getItem(localStateKey("username")) || "null"
+  );
 
   const response = await axios.post(apiUrl, {
     username,
